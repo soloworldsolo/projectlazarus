61ac70f7c17dfa1105d63e15b2beee955c6a878e
soloworldsolo <shaminsolomonit@gmail.com>
2021-09-25 21:30:55 +0530

    "adding response entity"

--- src/main/java/com/lazarus/twitter/projectlazarus/controller/TweetController.java
+++ src/main/java/com/lazarus/twitter/projectlazarus/controller/TweetController.java
@@ -4,11 +4,14 @@ import com.lazarus.twitter.projectlazarus.model.Status;
 import com.lazarus.twitter.projectlazarus.model.Tweet;
 import com.lazarus.twitter.projectlazarus.service.TweetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
-
+/**
+ * rest controller contains endpoints for all API
+ */
 @RestController
 public class TweetController {
     @Autowired
@@ -20,22 +23,22 @@ public class TweetController {
     }
 
     @PostMapping("/tweet/status")
-    public String postStatusUpdate(@RequestBody Status status) {
-        return tweetService.updateStatus(status);
+    public ResponseEntity postStatusUpdate(@RequestBody Status status) {
+        return ResponseEntity.ok(tweetService.updateStatus(status));
     }
 
     @DeleteMapping("/tweet/destroy/{id}")
-    public String destroyStatus(@PathVariable("id") String id) {
-        return tweetService.deleteStatus(id);
+    public ResponseEntity destroyStatus(@PathVariable("id") String id) {
+        return ResponseEntity.ok(tweetService.deleteStatus(id));
     }
 
     @PostMapping("/tweet/retweet/{id}/{tweetId}")
-    public String reTweet(@PathVariable("id") String id ,@PathVariable("tweetId") String tweetId){
-        return tweetService.reTweet(id,tweetId);
+    public ResponseEntity reTweet(@PathVariable("id") String id, @PathVariable("tweetId") String tweetId) {
+        return ResponseEntity.ok().body(tweetService.reTweet(id, tweetId));
     }
 
     @PutMapping("/tweet/hidereply/{id}")
-    public String hideReplies(@PathVariable("id") String id ,@RequestBody Status status){
-        return tweetService.hideReplies(id ,status);
+    public ResponseEntity hideReplies(@PathVariable("id") String id, @RequestBody Status status) {
+        return ResponseEntity.ok().body(tweetService.hideReplies(id, status));
     }
 }
